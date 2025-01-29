03364e805e08b554c8e37d90585734379507a631
Master <master@srvuisrael.edu.ec>
2025-01-28 20:05:38 -0500

    Initail commit

---
+++ src/main/java/com/uisrael/legalprosglpweb/LegalprosglpwebApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.legalprosglpweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalprosglpwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalprosglpwebApplication.class, args);
+	}
+
+}
